b052d4479f19518f120f7392fa8219baca30aaa5
Sofia Singam <sofiasingam@gmail.com>
2024-05-14 12:59:59 +0100

    created seed

--- db/migrate/20240513151853_add_collection_to_media.rb
+++
@@ -1,5 +0,0 @@
-class AddCollectionToMedia < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :media, :collection, null: false, foreign_key: true
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts "Generating users.."
+sofia = User.new(username: "Szcs22", bio: "Test User #1")
+sofia.save!
+morithi = User.new(username: "morithii", bio: "Test User #2")
+morithi.save!
+conor = User.new(username: "mcdonald-conor", bio: "Test User #3")
+conor.save!
+puts "Finished generating users!"
+
+puts "Generating 5 fake collections..."
+5.times.do
+  collection = Collection.new(
+    user_id: rand(1..3)
+    title: Faker::Lorem.word
+    description: Faker::Lorem.paragraph
+  )
+  collection.save!
+end
+puts "Finished generating collections!"
+
+puts "Generating 10 fake mediums..."
+10.times.do
+  media = Media.new(
+    user_id: rand(1..3)
+    category: ["Book","Article","Video"].sample
+    description: Faker::Lorem.paragraph
+    collection_id: rand(1..5)
+    author: Faker::Book.author
+    title: Faker::Book.title
+  )
+  media.save!
+end
+puts "Finished generating mediums!"
+
+puts "Finished!"
