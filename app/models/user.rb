44d479170886a582655b3024e96f715702a5d0eb
Sofia Singam <sofiasingam@gmail.com>
2024-05-20 17:58:09 +0100

    users can now add and edit avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :media
-
+  has_one_attached :avatar
 end
