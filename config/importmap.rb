55a22e346d4e07eefdbabf824d540a914e53e366
Sofia Singam <sofiasingam@gmail.com>
2024-05-22 18:16:00 +0100

    sweet alter when loading search

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "stimulus" # @3.2.2
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.2/dist/sweetalert2.all.js"
